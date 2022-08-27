5e61c1e64bd6f2b2726cfb6eee9cd05af3dbab9a
sagawatatsuya <zdaye0969@gmail.com>
2022-08-27 19:50:58 +0900

    update

--- README.md
+++ README.md
@@ -1,3 +1,118 @@
 # transformer-chemical-reaction-prediciton
-We trained T5 and DeBERTa on SMILES from ZINC and PubChem-10m using the task of masked-language modeling (MLM). These models can be used for the prediction of molecules' property, reaction, or interaction with proteins by changing the way of finetuning. Here, we conducted chemical reaction prediction, where if models were given a product, they generate reactants witch is nessesary for the reaction.
+We trained T5 and DeBERTa on SMILES from ZINC and PubChem-10m using the task of masked-language modeling (MLM). These models can be used for the prediction of molecules' property, reaction, or interaction with proteins by changing the way of finetuning. Here, we conducted a chemical reaction prediction, where if models were given a product, they generate reactants which is nessesary for the reaction.
 # How to start with
+The data used for pretraining and finetuning are uploaded to google drive, and you can find them at following links. ([ZINC](https://drive.google.com/drive/folders/1lSPCqh31zxTVEhuiPde7W3rZG8kPgp-z), [PubChem-10m](https://drive.google.com/file/d/1ygYs8dy1-vxD1Vx6Ux7ftrXwZctFjpV3/view), [ORD](https://drive.google.com/file/d/1BEk2GWhNU-Azj9hm77Z2wufsPN49wN0m/view))
+By executing the following command, you can download necessary data and preprocess them in one go.
+```
+python data-download-and-preprocess.py
+```
+Then, by executing the following command, you can complete preparation for model pretraining and finetuning.
+```
+python data-split-and-model-preparation.py
+```
+# Model pretraining
+If you want to redo model pretraining by yourself, you can do easily.
+
+PubChem10m-t5
+```
+cd /pretraining/PubChem10m-t5
+python ./new_run_t5_mlm_flax.py \
+    --output_dir="./PubChem10m-t5-base-output" \
+    --model_type="t5" \
+    --config_name="./PubChem10m-t5-base" \
+    --tokenizer_name="./PubChem10m-t5-base" \
+    --train_file="../../data/pubchem-10m-canonicalized-train.csv" \
+    --validation_file="../../data/pubchem-10m-canonicalized-valid.csv" \
+    --max_seq_length="512" \
+    --per_device_train_batch_size="5" \
+    --per_device_eval_batch_size="5" \
+    --adafactor \
+    --learning_rate="0.005" \
+    --weight_decay="0.001" \
+    --warmup_steps="2000" \
+    --overwrite_output_dir \
+    --logging_steps="500" \
+    --save_steps="10000" \
+    --num_train_epochs="30" \
+    --do_train \
+    --do_eval \
+    --eval_steps="25000"
+```
+
+ZINC-t5
+```
+cd /pretraining/ZINC-t5
+XLA_PYTHON_CLIENT_MEM_FRACTION=.8 python ./new_run_t5_mlm_flax.py \
+    --output_dir="./ZINC-t5-base-output" \
+    --model_type="t5" \
+    --config_name="./ZINC-t5-base" \
+    --tokenizer_name="./ZINC-t5-base" \
+    --train_file="../../data/ZINC-canonicalized-train.csv" \
+    --validation_file="../../data/ZINC-canonicalized-valid.csv" \
+    --max_seq_length="512" \
+    --per_device_train_batch_size="5" \
+    --per_device_eval_batch_size="5" \
+    --adafactor \
+    --learning_rate="0.005" \
+    --weight_decay="0.001" \
+    --warmup_steps="2000" \
+    --overwrite_output_dir \
+    --logging_steps="500" \
+    --save_steps="10000" \
+    --num_train_epochs="30" \
+    --do_train \
+    --do_eval \
+    --eval_steps="25000"
+```
+
+PubChem10m-deberta
+```
+cd /pretraining/PubChem10m-deberta
+python ./run_mlm.py \
+    --model_name_or_path "microsoft/deberta-base" \
+    --tokenizer_name "./PubChem10m-deberta-base" \
+    --num_train_epochs 30 \
+    --train_file "../../data/pubchem-10m-train.json" \
+    --validation_file "../../data/pubchem-10m-valid.json" \
+    --per_device_train_batch_size 5 \
+    --max_seq_length 512 \
+    --do_train \
+    --do_eval \
+    --output_dir "./PubChem10m-deberta-base-output" \
+    --overwrite_output_dir \
+    --evaluation_strategy "steps" \
+    --eval_steps 25000 \
+    --save_strategy "no" \
+    --logging_steps 200 \
+    --learning_rate 0.005 \
+    --report_to "none" \
+    --use_new_implementation \
+    --load_best_model_at_end True \
+    --disable_tqdm True
+```
+
+
+```
+cd /pretraining/ZINC-deberta
+python ./run_mlm.py \
+    --model_name_or_path "microsoft/deberta-base" \
+    --tokenizer_name "./ZINC-deberta-base" \
+    --num_train_epochs 30 \
+    --train_file "../../data/ZINC-train.json" \
+    --validation_file "../../data/ZINC-valid.json" \
+    --per_device_train_batch_size 5 \
+    --max_seq_length 512 \
+    --do_train \
+    --do_eval \
+    --output_dir "./ZINC-deberta-base-output" \
+    --overwrite_output_dir \
+    --evaluation_strategy "steps" \
+    --eval_steps 25000 \
+    --save_strategy "no" \
+    --logging_steps 200 \
+    --learning_rate 0.005 \
+    --report_to "none" \
+    --use_new_implementation \
+    --load_best_model_at_end True \
+    --disable_tqdm True
+```
---
+++ data-split-and-model-preparation.py
@@ -0,0 +1,283 @@
+import os
+import json
+import shutil
+import random
+import scipy as sp
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import torch
+import tokenizers
+import transformers
+from transformers import AutoTokenizer, T5Config
+from datasets import load_dataset
+%env TOKENIZERS_PARALLELISM=true
+import sentencepiece
+
+def seed_everything(seed=42):
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    
+seed_everything(seed=42)
+
+# ORD data
+# Prepare train, validation and test data
+all = pd.read_csv('./data/all_ord_reaction_uniq_canonicalized.csv')
+
+train, test = train_test_split(all, test_size=int(len(all)*0.1))
+train, val = train_test_split(train, test_size=int(len(all)*0.1))
+
+train.to_csv('./data/all_ord_reaction_uniq_canonicalized-train.csv', index=False)
+val.to_csv('./data/all_ord_reaction_uniq_canonicalized-valid.csv', index=False)
+test.to_csv('./data/all_ord_reaction_uniq_canonicalized-test.csv', index=False)
+
+
+# PubChem10m data
+
+# Initialize a dataset
+dataset = load_dataset('csv',data_files='./data/pubchem-10m-canonicalized.csv')
+training_corpus = (
+    dataset['train'][i : i + 1000]['smiles']
+    for i in range(0, len(dataset['train']), 1000)
+)
+
+# Train tokenizer
+old_tokenizer = AutoTokenizer.from_pretrained('microsoft/deberta-base')
+tokenizer = old_tokenizer.train_new_from_iterator(training_corpus, 1000)
+
+# Save files to disk
+tokenizer.save_pretrained('./pretraining/PubChem10m-deberta/PubChem10m-deberta-base')
+
+# Split into train and validation data
+all = pd.read_csv('./data/pubchem-10m-canonicalized.csv')
+train, valid = train_test_split(all, test_size=0.1)
+
+# Save train and validation data
+train.to_csv('./data/pubchem-10m-canonicalized-train.csv', index=False)
+valid.to_csv('./data/pubchem-10m-canonicalized-valid.csv', index=False)
+
+with open('./data/pubchem-10m-train.json', 'w') as fp:
+    for smiles in train.smiles:
+        fp.write(json.dumps({'text':smiles})+'\n')
+
+with open('./data/pubchem-10m-valid.json', 'w') as fp:
+    for smiles in valid.smiles:
+        fp.write(json.dumps({'text':smiles})+'\n')
+        
+        
+# ZINC data
+
+# Initialize a dataset
+dataset = load_dataset('csv',data_files='./data/ZINC-canonicalized.csv')
+training_corpus = (
+    dataset["train"][i : i + 1000]['text']
+    for i in range(0, len(dataset["train"]), 1000)
+)
+
+# Train tokenizer
+old_tokenizer = AutoTokenizer.from_pretrained('microsoft/deberta-base')
+tokenizer = old_tokenizer.train_new_from_iterator(training_corpus, 1000)
+
+# Save files to disk
+tokenizer.save_pretrained('./pretraining/ZINC-deberta/ZINC-deberta-base')
+
+# Split into train and validation data
+all = pd.read_csv('./data/ZINC-canonicalized.csv')
+train, valid = train_test_split(all, test_size=0.1)
+
+# Save train and validation data
+train.to_csv('./data/ZINC-canonicalized-train.csv', index=False)
+valid.to_csv('./data/ZINC-canonicalized-valid.csv', index=False)
+
+with open("./data/ZINC-train.json", "w") as fp:
+    for smiles in train.text:
+        fp.write(json.dumps({"text":smiles})+'\n')
+
+with open("./data/ZINC-valid.json", "w") as fp:
+    for smiles in valid.text:
+        fp.write(json.dumps({"text":smiles})+'\n')
+        
+        
+# https://github.com/huggingface/transformers/blob/main/examples/flax/language-modeling/t5_tokenizer_model.py
+#!/usr/bin/env python3
+import json
+from typing import Iterator, List, Union
+
+from tokenizers import AddedToken, Regex, Tokenizer, decoders, normalizers, pre_tokenizers, trainers
+from tokenizers.implementations.base_tokenizer import BaseTokenizer
+from tokenizers.models import Unigram
+from tokenizers.processors import TemplateProcessing
+
+
+class SentencePieceUnigramTokenizer(BaseTokenizer):
+    """
+    This class is a copy of `DeDLOC's tokenizer implementation <https://github.com/yandex-research/DeDLOC/blob/main/sahajbert/tokenizer/tokenizer_model.py>`__ .
+
+    Custom SentencePiece Unigram Tokenizer with NMT, NKFC, spaces and lower-casing characters normalization
+    Represents the Unigram algorithm, with the pretokenization used by SentencePiece
+    """
+
+    def __init__(
+        self,
+        replacement: str = "▁",
+        add_prefix_space: bool = True,
+        unk_token: Union[str, AddedToken] = "<unk>",
+        eos_token: Union[str, AddedToken] = "</s>",
+        pad_token: Union[str, AddedToken] = "<pad>",
+    ):
+        self.special_tokens = {
+            "pad": {"id": 0, "token": pad_token},
+            "eos": {"id": 1, "token": eos_token},
+            "unk": {"id": 2, "token": unk_token},
+        }
+
+        self.special_tokens_list = [None] * len(self.special_tokens)
+        for token_dict in self.special_tokens.values():
+            self.special_tokens_list[token_dict["id"]] = token_dict["token"]
+
+        tokenizer = Tokenizer(Unigram())
+
+        tokenizer.normalizer = normalizers.Sequence(
+            [
+                normalizers.Nmt(),
+                normalizers.NFKC(),
+                normalizers.Replace(Regex(" {2,}"), " "),
+#                 normalizers.Lowercase(),
+            ]
+        )
+        tokenizer.pre_tokenizer = pre_tokenizers.Sequence(
+            [
+                pre_tokenizers.Metaspace(replacement=replacement, add_prefix_space=add_prefix_space),
+                pre_tokenizers.Digits(individual_digits=True),
+                pre_tokenizers.Punctuation(),
+            ]
+        )
+        tokenizer.decoder = decoders.Metaspace(replacement=replacement, add_prefix_space=add_prefix_space)
+
+        tokenizer.post_processor = TemplateProcessing(
+            single=f"$A {self.special_tokens['eos']['token']}",
+            special_tokens=[(self.special_tokens["eos"]["token"], self.special_tokens["eos"]["id"])],
+        )
+
+        parameters = {
+            "model": "SentencePieceUnigram",
+            "replacement": replacement,
+            "add_prefix_space": add_prefix_space,
+        }
+
+        super().__init__(tokenizer, parameters)
+
+    def train(
+        self,
+        files: Union[str, List[str]],
+        vocab_size: int = 8000,
+        show_progress: bool = True,
+    ):
+        """Train the model using the given files"""
+
+        trainer = trainers.UnigramTrainer(
+            vocab_size=vocab_size,
+            special_tokens=self.special_tokens_list,
+            show_progress=show_progress,
+        )
+
+        if isinstance(files, str):
+            files = [files]
+        self._tokenizer.train(files, trainer=trainer)
+
+        self.add_unk_id()
+
+    def train_from_iterator(
+        self,
+        iterator: Union[Iterator[str], Iterator[Iterator[str]]],
+        vocab_size: int = 8000,
+        show_progress: bool = True,
+    ):
+        """Train the model using the given iterator"""
+
+        trainer = trainers.UnigramTrainer(
+            vocab_size=vocab_size,
+            special_tokens=self.special_tokens_list,
+            show_progress=show_progress,
+        )
+
+        self._tokenizer.train_from_iterator(iterator, trainer=trainer)
+
+        self.add_unk_id()
+
+    def add_unk_id(self):
+        tokenizer_json = json.loads(self._tokenizer.to_str())
+
+        tokenizer_json["model"]["unk_id"] = self.special_tokens["unk"]["id"]
+
+        self._tokenizer = Tokenizer.from_str(json.dumps(tokenizer_json))
+        
+        
+# PubChem10m data
+
+vocab_size = 32_000
+input_sentence_size = None
+
+# Initialize a dataset
+dataset = load_dataset('csv',data_files='./data/pubchem-10m-canonicalized.csv')
+
+tokenizer = SentencePieceUnigramTokenizer(unk_token="<unk>", eos_token="</s>", pad_token="<pad>")
+
+# Build an iterator over this dataset
+def batch_iterator(input_sentence_size=None):
+    if input_sentence_size is None:
+        input_sentence_size = len(dataset)
+    batch_length = 1000
+    for i in range(0, input_sentence_size, batch_length):
+        yield dataset["train"][i: i + batch_length]['smiles']
+
+# Train tokenizer
+tokenizer.train_from_iterator(
+    iterator=batch_iterator(input_sentence_size=input_sentence_size),
+    vocab_size=vocab_size,
+    show_progress=True,
+)
+
+# Save files to disk
+tokenizer.save('./pretraining/PubChem10m-t5/PubChem10m-t5-base/tokenizer.json')
+
+config = T5Config.from_pretrained('google/t5-v1_1-base', vocab_size=tokenizer.get_vocab_size())
+config.save_pretrained('./pretraining/PubChem10m-t5/PubChem10m-t5-base/')
+
+
+# ZINC data
+
+vocab_size = 32_000
+input_sentence_size = None
+
+# Initialize a dataset
+dataset = load_dataset('csv',data_files='./data/ZINC-canonicalized.csv')
+
+tokenizer = SentencePieceUnigramTokenizer(unk_token="<unk>", eos_token="</s>", pad_token="<pad>")
+
+# Build an iterator over this dataset
+def batch_iterator(input_sentence_size=None):
+    if input_sentence_size is None:
+        input_sentence_size = len(dataset)
+    batch_length = 1000
+    for i in range(0, input_sentence_size, batch_length):
+        yield dataset["train"][i: i + batch_length]['text']
+
+# Train tokenizer
+tokenizer.train_from_iterator(
+    iterator=batch_iterator(input_sentence_size=input_sentence_size),
+    vocab_size=vocab_size,
+    show_progress=True,
+)
+
+# Save files to disk
+tokenizer.save('./pretraining/ZINC-t5/ZINC-t5-base/tokenizer.json')
+
+config = T5Config.from_pretrained('google/t5-v1_1-base', vocab_size=tokenizer.get_vocab_size())
+config.save_pretrained('./pretraining/ZINC-t5/ZINC-t5-base/')
+
+
